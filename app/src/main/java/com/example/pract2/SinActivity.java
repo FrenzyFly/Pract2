2cd1eed6eb543ee6395f8c4eb124b52f93688692
Daniil <firefly.daniil@gmail.com>
2023-03-16 21:53:11 +0300

    Twentieth commit

--- app/src/main/java/com/example/pract2/SinActivity.java
+++ app/src/main/java/com/example/pract2/SinActivity.java
@@ -1,13 +1,7 @@
 package com.example.pract2;
 
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -23,7 +17,7 @@ public class SinActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sin);
+        setContentView(R.layout.fragment_sin);
 
         TextView sign_in_text = findViewById(R.id.signin_text);
         sign_in_text.setText("Sign In");
