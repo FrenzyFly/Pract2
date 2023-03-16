beb3b4c3edca6597c92e70986d7ac400c7ee080f
Daniil <firefly.daniil@gmail.com>
2023-03-16 22:30:22 +0300

    ...

---
+++ app/src/main/java/com/example/pract2/SActivity.java
@@ -0,0 +1,13 @@
+package com.example.pract2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_s);
+    }
+}
